adcda35b08eb489706143bf32c52d4d5cfd679f3
Johodah <johnny.dahle@hotmail.com>
2023-11-10 09:34:50 +0100

    Lagt in större delen av kod for now

---
+++ Scripts/scripts.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+loadPrograms();
+});
+
+function addProgram() {
+    var title = document.getElementById("programTitle").value;
+    var description = document.getElementById("programBeskrivning").value;
+    var ageLimit = document.getElementById("ageLimit").value;
+
+    var newProgram = {
+        title: title,
+        description: description,
+        ageLimit: ageLimit,
+    };
+}
+
+var programs = JSON.parse(localStorage.getItem("programs")) || [];
+
+
+programs.push(newProgram);
+
+localStorage.setItem("programs", JSON.stringify(programs));
+
+loadPrograms();
+
+document.getElementById("programForm").reset();
+
+
+function loadPrograms() {
+    var programList = document.getElementById("programList");
+    programList.innerHTML = '';
+
+    var programs = JSON.parse(localStorage.getItem("programs")) || [];
+
+    programs.forEach(function (program) {
+        var li = document.createElement("li");
+        li.innerHTML = "<strong>${program.title}</strong> (${program.ageLimit}+) - ${program.description}";
+        programList.appendChild(li);
+    });
+}
+
+function searchPrograms() {
+    var searchInput = document.getElementById("searchProgram").value.toLowerCase();
+    var programList = document.getElementById("programList");
+
+    var programs = JSON.parse(localStorage.getItem("programs")) || [];
+
+    var filteredPrograms = programs.filter(function (program) {
+        return program.title.toLowerCase(),includes(searchInput) || program.description.toLowerCase(),includes(searchInput);  
+    });
+}
